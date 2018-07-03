b3239a303bf1bbca3310f09831423959bf320aa8
wwwal2 <smalex9@gmail.com>
2018-07-03 18:05:08 +0300

    finished app

--- app.js
+++ app.js
@@ -8,22 +8,28 @@ console.log(helpFromHdd);
 const parsedYargs = helpFromNPM.argv;
 console.log('Yargs: ', parsedYargs);
 
-var userInput = parsedYargs._[0];
+let userInput = parsedYargs._[0];
 console.log('User input:', userInput);
 
 if (userInput === 'add') {
-    var responded = helpFromHdd.addNote(parsedYargs.title, parsedYargs.body);
-    if (responded === undefined) {
-        console.log('Nothing added');
-    } else {
-        console.log('Note added: ', parsedYargs.title)
-    }
+    let respondedA = helpFromHdd.addNote(parsedYargs.title, parsedYargs.body);
+    let answerA = respondedA ? `Note added: ${parsedYargs.title}` : 'Nothing added';
+    console.log(answerA);
+
 } else if (userInput === 'remove') {
-    console.log('Note removed');
+    let respondedR = helpFromHdd.removeNote(parsedYargs.title);
+    let answerR = respondedR ? `Note ${parsedYargs.title} removed` : `Note ${parsedYargs.title} not found`;
+    console.log(answerR);
+
 } else if (userInput === 'getNote') {
-    console.log('Note: ');
+    let respondG = helpFromHdd.getNote(parsedYargs.title);
+    let answerG = respondG ? `Note: ${respondG.title} ${respondG.body}` : `Note ${parsedYargs.title} not found`;
+    console.log(answerG);
+
 } else if (userInput === 'getAll') {
-    console.log('Getting all notes: ');
+    let respondGA = helpFromHdd.getAll();
+    respondGA.forEach(function (item, i, arr) {console.log(i, item.title, item.body);});
+
 } else {
     console.log('No known command used');
 };
--- notes.js
+++ notes.js
@@ -2,29 +2,29 @@ const helpFromSpaceLocal = require('fs');
 
 console.log('notes.js attached');
 
-var saveToData = (insertObject) => {
+let saveToData = (insertObject) => {
     helpFromSpaceLocal.writeFileSync('./data_stash/data.json', JSON.stringify(insertObject));
 };
 
-var duplicateCheck = () => {
+let duplicateCheck = () => {
 
     try {
-        var enlargeNoteList = helpFromSpaceLocal.readFileSync('./data_stash/data.json');
+        let enlargeNoteList = helpFromSpaceLocal.readFileSync('./data_stash/data.json');
         return JSON.parse(enlargeNoteList);
     } catch (e) {
         return []
     }
 };
 
-var addNote = (title, body) => {
+let addNote = (title, body) => {
 
-    var notesArray = duplicateCheck();
-    var oneNote = {
+    let notesArray = duplicateCheck();
+    let oneNote = {
         title,
         body
     };
 
-    var filterDuplicateNotes = notesArray.filter((duplicates) => {return duplicates.title === title;});
+    let filterDuplicateNotes = notesArray.filter((duplicates) => {return duplicates.title === title });
 
     if (filterDuplicateNotes.length === 0) {
         notesArray.push(oneNote);
@@ -33,6 +33,28 @@ var addNote = (title, body) => {
     }
 };
 
+let removeNote = (title) => {
+    let getListR = duplicateCheck();
+    let newListWithoutSelected = getListR.filter((duplicates) => {return duplicates.title !== title });
+    saveToData(newListWithoutSelected);
+
+    return getListR.length !== newListWithoutSelected
+};
+
+let getNote = (title) => {
+    let getListG = duplicateCheck();
+    let selectedElement = getListG.filter((duplicates) => {return duplicates.title === title });
+    return selectedElement[0];
+};
+
+let getAll = () => {
+    let allNotes = duplicateCheck();
+    return allNotes;
+};
+
 module.exports = {
     addNote,
+    removeNote,
+    getNote,
+    getAll
 };
